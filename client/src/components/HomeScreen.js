1768a8276c345309694bf626883b13fec1a3414c
Gemju Sherpa <gemju.s1@gmail.com>
2020-11-30 13:35:38 +1000

    Product display on homescreen added

---
+++ client/src/components/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Products from '../products'
+import Product from '../components/Product'
+import {Row, Col} from 'react-bootstrap'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {Products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} lg={4}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen

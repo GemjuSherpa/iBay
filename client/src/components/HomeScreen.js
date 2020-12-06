76044480ec00556a6971780ba26207b70817ef13
Gemju Sherpa <gemju.s1@gmail.com>
2020-12-07 08:44:26 +1000

    Spinner and error message component added

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -3,7 +3,8 @@ import {useDispatch, useSelector} from 'react-redux'
 import Product from '../components/Product'
 import {Row, Col} from 'react-bootstrap'
 import {listProducts} from '../actions/productActions'
-
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 
 
@@ -21,7 +22,7 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? <h1>Loading .. </h1>: error ? <h3>{error}</h3> : 
+            {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : 
             <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4}>

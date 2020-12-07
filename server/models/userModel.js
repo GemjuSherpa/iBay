b790839d451c1bbe3fe90278855d40e80b11bc53
Gemju Sherpa <gemju.s1@gmail.com>
2020-12-07 14:59:54 +1000

    User registration API added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -25,10 +25,23 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+// decrypt the password and match
 userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password)
     
 }
+
+// encrypt password before creating new user
+userSchema.pre('save', async function(next){
+    // check if the password has been modified already
+    if(!this.isModified('password')){
+        next()
+    }
+    
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
 
 // create user table
 const User = mongoose.model('User', userSchema)

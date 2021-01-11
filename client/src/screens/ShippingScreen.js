cf3f1966a2c94e9e305d6df88ed98cbf859f3940
Gemju Sherpa <gemju.s1@gmail.com>
2021-01-12 09:31:12 +1000

    checkout steps nav component added

--- client/src/screens/ShippingScreen.js
+++ client/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import {Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import {saveShippingAddress} from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 
 const ShippingScreen = ({history}) => {
@@ -33,56 +34,57 @@ const ShippingScreen = ({history}) => {
 
     return (
         <FormContainer>
-            <h1>Shipping</h1>
-            <Form onSubmit = {submitHandler}>
-                <Form.Group controlId='address'>
-                   <Form.Label>Address</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder="Enter your address"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    >
-                    </Form.Control>
-               </Form.Group>
+            <CheckoutSteps step1 step2/>
+                <h1>Shipping</h1>
+                <Form onSubmit = {submitHandler}>
+                    <Form.Group controlId='address'>
+                    <Form.Label>Address</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder="Enter your address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        >
+                        </Form.Control>
+                </Form.Group>
 
-               <Form.Group controlId='city'>
-                   <Form.Label>City</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder="Enter City"
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    >
-                    </Form.Control>
-               </Form.Group>
+                <Form.Group controlId='city'>
+                    <Form.Label>City</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder="Enter City"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        >
+                        </Form.Control>
+                </Form.Group>
 
-               <Form.Group controlId='postalCode'>
-                   <Form.Label>Postal Code</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder="Enter your postal code"
-                        value={postalCode}
-                        onChange={(e) => setPostalCode(e.target.value)}
-                    >
-                    </Form.Control>
-               </Form.Group>
+                <Form.Group controlId='postalCode'>
+                    <Form.Label>Postal Code</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder="Enter your postal code"
+                            value={postalCode}
+                            onChange={(e) => setPostalCode(e.target.value)}
+                        >
+                        </Form.Control>
+                </Form.Group>
 
-               <Form.Group controlId='country'>
-                   <Form.Label>Country</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder="Enter your country"
-                        value={country}
-                        onChange={(e) => setCountry(e.target.value)}
-                    >
-                    </Form.Control>
-               </Form.Group>
+                <Form.Group controlId='country'>
+                    <Form.Label>Country</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder="Enter your country"
+                            value={country}
+                            onChange={(e) => setCountry(e.target.value)}
+                        >
+                        </Form.Control>
+                </Form.Group>
 
-               <Button type='submit' variant='primary'>
-                   Continue
-               </Button>
-            </Form>
+                <Button type='submit' variant='primary'>
+                    Continue
+                </Button>
+                </Form>
         </FormContainer>
     )
 }
